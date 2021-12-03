b38cc93793e708e5c7c46e3694ce3179bd08b4cc
Branson Ng <67825891+bransoonnn@users.noreply.github.com>
2021-12-03 13:50:07 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
+
+    function myFunction() {
+  alert("Thank You for contacting! I will get back to you soon!");
+  }
